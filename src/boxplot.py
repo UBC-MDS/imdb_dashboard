ed19e988d9f3b724f0ae054e98129d8f9fbb643f
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-03-05 12:43:18 -0800

    feat: add color legend to boxplot

--- src/boxplot.py
+++ src/boxplot.py
@@ -25,8 +25,10 @@ def generate_box_plot(data: pd.DataFrame):
     chart = alt.Chart(data).mark_boxplot(size=25).encode(
         x=alt.X('genres',
                 axis=alt.Axis(title="Genre", labelAngle=-45)),
-        y=alt.Y('averageRating',
-                axis=alt.Axis(title="IMDB Rating")),
+        y=alt.Y('averageRating', title="IMDb Rating"),
+        color=alt.Color('genres',
+                        title="Genres",
+                        scale=alt.Scale(scheme='darkgold'))
     )
 
     return chart.configure_axis(
